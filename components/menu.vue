8219e66df34ac30b57944d085c9fb7da2a34c2bc
hongying2018 <hongying@ds.cn>
2019-03-06 11:21:25 +0800

    second commit

---
+++ components/menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="menulist">
+    <nuxt-link to="/detail">detail</nuxt-link>
+    <nuxt-link to="/introduce">introduce</nuxt-link>
+    <nuxt-link to="/main">main</nuxt-link>
+  </div>
+</template>
+
+<style>
+
+
+</style>
